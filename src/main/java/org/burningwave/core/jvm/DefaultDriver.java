f3745c48591d11ad0e4a49723870965c191a38a0
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-06-25 10:48:34 +0200

    Update

--- src/main/java/org/burningwave/core/jvm/DefaultDriver.java
+++ src/main/java/org/burningwave/core/jvm/DefaultDriver.java
@@ -724,7 +724,6 @@ public class DefaultDriver extends Driver {
 			@Override
 			protected void initMainConsulter() {
 				super.initMainConsulter();
-				//driver.unsafe.putInt(mainConsulter, 12L, -1);
 			}
 			
 			@Override
